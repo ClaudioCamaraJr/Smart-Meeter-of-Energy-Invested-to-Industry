c087b989426b12668245ef71a400ce2e82b33bc2
ClaudioCamaraJr <31418734+ClaudioCamaraJr@users.noreply.github.com>
2017-11-20 14:32:51 -0200

    Licença

--- MainModule.X/nbproject/Makefile-impl.mk
+++ MainModule.X/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=testeTCC.X
+PROJECTNAME=MainModule.X
 
 # Active Configuration
 DEFAULTCONF=default
--- MainModule.X/nbproject/Makefile-variables.mk
+++ MainModule.X/nbproject/Makefile-variables.mk
@@ -6,8 +6,8 @@
 CND_BASEDIR=`pwd`
 # default configuration
 CND_ARTIFACT_DIR_default=dist/default/production
-CND_ARTIFACT_NAME_default=testeTCC.X.production.hex
-CND_ARTIFACT_PATH_default=dist/default/production/testeTCC.X.production.hex
+CND_ARTIFACT_NAME_default=MainModule.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/MainModule.X.production.hex
 CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
-CND_PACKAGE_NAME_default=testetcc.x.tar
-CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/testetcc.x.tar
+CND_PACKAGE_NAME_default=mainmodule.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/mainmodule.x.tar
